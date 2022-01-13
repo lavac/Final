4ba9de78ebce07e0275d5803da5ad6db62627329
Lavanya C <lavanyacs743@gmail.com>
2022-01-13 17:15:50 +0530

    Render Header conditionally and fix css

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,53 @@
 import React, {useEffect, useState} from "react";
+import BasicModal from '../../screens/Login/Login';
+import Button from '@mui/material/Button';
 
 import logo from "../../assets/logo.svg";
 
 import "./header.css";
 
 
-const Screen = (props) => {
+const button = {
+  width: 64,
+  height: 32,
+  variant: 'contained',
+  color: 'black',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+};
 
+const bookShow = {
+  width: 120,
+  height: 32
+};
+
+
+class Header extends React.Component {
+
+  constructor(props)
+  {
+      super(props);
+
+      this.state = {isLoggedIn : false};
+
+      this.ifLoginClicked = this.ifLoginClicked.bind(this);
+      this.ifLogoutClicked = this.ifLogoutClicked.bind(this);
+  }
+
+  ifLoginClicked()
+  {
+      this.setState({isLoggedIn : true});
+      // this.setState({isTryingToLog : true});
+
+  }
+
+  ifLogoutClicked()
+  {
+      this.setState({isLoggedIn : false});
+  }
+  
+
+  render() {
   return (
 
     <div>
@@ -14,18 +55,23 @@ const Screen = (props) => {
         <div className="header">
           <img className="logo" src={logo}/>
           <div className="header-right">
-            <a className="active" href="#home">Login</a>
+                {
+                    (this.state.isLoggedIn)? <div className="loggedInState">
+                    <Button sx={bookShow} variant="contained">BOOK SHOW</Button>
+                    {/* <Logout clickFunc = {this.ifLogoutClicked} /> */}
+                    <Button sx={button} variant="contained" onClick={this.ifLogoutClicked} >LOGOUT</Button>
+
+                    </div>:
+                      <BasicModal ifLoginClicked = {this.ifLoginClicked}/>
+                } 
           </div>
         </div>
-
-
       </header>
       <div className="upcomingMovies"> Upcoming Movies </div>
     </div>
 
   )
-
+  }
 }
 
-
-export default Screen;
+export default Header;
--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,14 +1,61 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import {Modal, TextField, Stack} from '@mui/material';
+import color from '@material-ui/core/colors/amber';
 
-class Login extends React.Component {
-  render() {
-    return (
-      <div>
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
+const button = {
+  width: 64,
+  height: 32,
+  variant: 'contained',
+  color: 'black',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+};
 
-      </div>
-    )
-  }
+export default function BasicModal(props) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button sx={button} onClick={handleOpen}>Login</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          {/* <Typography id="modal-modal-title" variant="h6" component="h2"> */}
+          <Stack
+      component="form"
+      sx={{
+        width: '25ch',
+      }}
+      spacing={2}
+      noValidate
+      autoComplete="off"
+    >
+          <TextField id="filled-basic" label="Username*" variant="filled" />
+          <TextField id="filled-basic" label="Password*" variant="filled" />
+          <Button variant="contained" onClick={props.ifLoginClicked} >LOGIN</Button>
+          </Stack>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
-
-export default Login;
